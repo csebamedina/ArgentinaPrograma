27500b61b4eb7d2d53f924e1f04845aefc5277dc
csebamedina <csebamedina@hotmail.com>
2022-08-23 13:56:24 -0300

    FrontEnd

---
+++ ArgentinaPrograma/BackEnd/src/main/java/com/portfolio/csm/CsmApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.csm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsmApplication.class, args);
+	}
+
+}
---
+++ ArgentinaPrograma/BackEnd/src/test/java/com/portfolio/csm/CsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.csm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
