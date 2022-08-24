2587496d0d1e441b208eceecc3b7e67870e71707
csebamedina <csebamedina@hotmail.com>
2022-08-24 11:03:00 -0300

    Inicio Backend

---
+++ ArgentinaPrograma/BackEnd/src/main/java/com/portfolio/csm/Interface/IPersonaService.java
@@ -0,0 +1,18 @@
+package com.portfolio.csm.Interface;
+
+import com.portfolio.csm.Entity.Persona;
+import java.util.List;
+
+public interface IPersonaService {
+    //Traer una lista de persona
+    public List<Persona> getPersona();
+    
+    //Guardar un objeto de tipo Persona
+    public void savePersona(Persona persona);
+
+    //Eliminar un objeto pero lo buscamos por ID
+    public void deletePersona(Long id);
+    
+    //Buscar una persona por ID
+    public Persona findPersona(Long id);
+}
