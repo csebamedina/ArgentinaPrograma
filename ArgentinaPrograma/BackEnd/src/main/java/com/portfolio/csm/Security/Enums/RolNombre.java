445dbd8caadbd192235e20cb10ac6be5a7681f74
csebamedina <csebamedina@hotmail.com>
2022-08-27 19:08:24 -0300

    FrontEnd & BackEnd

---
+++ ArgentinaPrograma/BackEnd/src/main/java/com/portfolio/csm/Security/Enums/RolNombre.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.csm.Security.Enums;
+
+/**
+ *
+ * @author Mauricio Medina
+ */
+public enum RolNombre {
+    ROLE_ADMIN, ROLE_USER
+}
